547f333a673845c22b8ca008f12c6b19d52efe6f
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2015-09-16 03:21:58 -0400

    Project config.

---
+++ language-implementations/tools.compiler/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tools.compiler
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ language-implementations/tools.compiler/project.clj
@@ -0,0 +1,29 @@
+;;
+;; Author José Albert Cruz Almaguer <jalbertcruz@gmail.com>
+;; Copyright 2015 by José Albert Cruz Almaguer.
+;;
+;; This program is licensed to you under the terms of version 3 of the
+;; GNU Affero General Public License. This program is distributed WITHOUT
+;; ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+;; MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+;; AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+;;
+
+(defproject language-implementations/tools.compiler "0.0.1-SNAPSHOT"
+
+  :description ""
+
+  :url "https://github.com/jalbertcruz/language-implementations/tree/master/tools.compiler"
+
+  :license {:name "GNU Affero General Public License"
+            :url "http:www.gnu.org/licenses/agpl-3.0.txt"}
+
+  :dependencies [
+                  [org.clojure/clojure "1.7.0"]
+                  [org.clojure/core.match "0.3.0-alpha4"]
+                  [de.ubercode.clostache/clostache "1.4.0"]
+                  ]
+
+  :main ^:skip-aot temp
+
+  )
